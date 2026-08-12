bbbbdec358d3d899ae756b53d0cf73216dabcdcf
JuanFCast <83439579+JuanFCast@users.noreply.github.com>
2026-08-12 16:00:26 -0500

    Redirect BeeSmart domain to BIntuitive

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,17 @@ const nextConfig: NextConfig = {
   async redirects() {
     return [
       {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "beesmart.aumcrsp.com",
+          },
+        ],
+        destination: "https://bintuitive.aumcrsp.com/:path*",
+        permanent: true,
+      },
+      {
         source: "/categorias",
         destination: "/hexagons",
         permanent: true,
